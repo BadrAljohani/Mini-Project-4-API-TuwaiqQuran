076e2d1a6525a7133408370e5ce452874e2038da
BadrAljohani <Badr.Aljohani.med.s@tuwaiq.edu.sa>
2021-12-10 22:19:01 +0300

    The pages that have complet is viewController and PlayerVC

--- TuwaiqQuran/PlayerVC.swift
+++ TuwaiqQuran/PlayerVC.swift
@@ -2,8 +2,8 @@ import UIKit
 import AVFoundation
 
 class PlayerVC: UIViewController {
-
-
+    
+    
     var radio_Play = ""
     var theNumber : Int = 0
     var soraAraay = ["adel"]
@@ -11,14 +11,14 @@ class PlayerVC: UIViewController {
     var radios : [Radio] = []
     
     @IBOutlet weak var sliders: UISlider!
-
-        
+    
+    
     @IBAction func sliderMov(_ seder: Any) {
         SoundPlayer.player.currentTime = TimeInterval(sliders.value)
         SoundPlayer.player.play()
     }
     
-   
+    
     var player : AVPlayer?
     @IBOutlet weak var soraName: UILabel!
     @IBOutlet weak var playnow: UIButton!
@@ -34,11 +34,11 @@ class PlayerVC: UIViewController {
     func nextNprev(){
         
         SoundPlayer.play(number: theNumber + 1 )
-               soraName.text = soraAraay[theNumber]
-//               playLable.setTitle("ايقاف", for: .normal)
-            playnow.setImage(UIImage(named: "pause.png"), for: .normal)
-                sliders.maximumValue = Float(SoundPlayer.player.duration)
-               sliders.value = 0
+        soraName.text = soraAraay[theNumber]
+        //               playLable.setTitle("ايقاف", for: .normal)
+        playnow.setImage(UIImage(named: "pause.png"), for: .normal)
+        sliders.maximumValue = Float(SoundPlayer.player.duration)
+        sliders.value = 0
         
     }
     @objc func timer() {
@@ -54,52 +54,52 @@ class PlayerVC: UIViewController {
         //soraName.textColor = UIColor.white
         //sliders.maximumValue = Float(SoundPlayer.player.duration)
         
-//        _ = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(PlayerVC.timer) , userInfo: nil, repeats: true)
-//
+        //        _ = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(PlayerVC.timer) , userInfo: nil, repeats: true)
+        //
     }
     
     
-
-        
+    
+    
     @IBAction func PlayPresed(_ sender: Any){
         
         isPlaying = !isPlaying
         
         if isPlaying {
-          
+            
             SoundPlayer.playAfterPause()
             playnow.setImage(UIImage(named: "pause.png"), for: .normal)
-                       
-         
+            
+            
             
         } else {
             
             playnow.setImage(UIImage(named: "play.png"), for: .normal)
-                     
-                     SoundPlayer.player.stop()
-             
+            
+            SoundPlayer.player.stop()
+            
         }
     }
     
+    
+    @IBAction func prevPresed(_ sender: Any){
         
-        @IBAction func prevPresed(_ sender: Any){
-        
-         if theNumber < 1 { return}
-         theNumber -= 1
-    nextNprev()
+        if theNumber < 1 { return}
+        theNumber -= 1
+        nextNprev()
     }
     
     @IBAction func nextPressed(_ sender: Any) {
         if theNumber > 112 {return }
         theNumber += 1
-       nextNprev()
+        nextNprev()
         
     }
     
     
-  
-        
-        @IBAction func backPresed(_ sender: Any){
+    
+    
+    @IBAction func backPresed(_ sender: Any){
         
         navigationController?.popToRootViewController(animated: true)
         
@@ -107,5 +107,5 @@ class PlayerVC: UIViewController {
         
     }
     
-
+    
 }
--- TuwaiqQuran/ViewController.swift
+++ TuwaiqQuran/ViewController.swift
@@ -4,128 +4,75 @@ import UIKit
 import Alamofire
 import AVFoundation
 
-
-var soraArray = [String]()
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     var player : AVPlayer?
     var radios : [Radio] = []
     let session = URLSession.shared
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         radios.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let Cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
         Cell.textLabel?.textAlignment = .left
         Cell.textLabel?.text = radios[indexPath.row].name
         Cell.detailTextLabel?.text = radios[indexPath.row].radio_url
-
+        
         return Cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        radio_Play
+        
         performSegue(withIdentifier: "sender", sender: self)
         
-
-//        let alert = UIAlertController(title: "Edit", message: nil, preferredStyle: .alert)
-               
-//                       // add textField in alert
-//               alert.addTextField()
-//
-//               var textBox = alert.textFields?.first
-////        textBox? = self.radios[indexPath.row]
-//
-//               alert.addAction(UIAlertAction(title: "Save", style: .default, handler: {action in
-//
-////                   self.radios[indexPath.row] = textBox?
-//               }))
-//               
-//               alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-//
-//
-//               DispatchQueue.main.async {
-//                   self.present(alert, animated: true, completion: nil)
-//           }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let index = tableView.indexPathForSelectedRow?.row
         let info = segue.destination as! PlayerVC
         info.radio_Play = radios[index!].radio_url
     }
-
-    @IBOutlet weak var tableView: UITableView! //{
-//        didSet {
-//            tableView.delegate = self ; tableView.dataSource = self
-//        }
-//    }
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self ; tableView.dataSource = self
         getData()
-//        playOneAyah()
         DispatchQueue.main.asyncAfter(deadline: .now() + 3){
             for item in self.radios {
                 print(item.name )
                 print(item.radio_url)
             }
         }
-        // Do any additional setup after loading the view.
-//        "http://live.mp3quran.net:9702/;"
     }
-
     
-
     func getData(){
         var ulrcomponent = URLComponents()
         ulrcomponent.scheme = "https"
         ulrcomponent.host = "api.mp3quran.net"
         ulrcomponent.path="/radios/radio_arabic.json"
-
+        
         let requset = URLRequest(url: ulrcomponent.url!)
         print(requset)
-
+        
         let task = session.dataTask(with: requset) { (data ,respon,err) in
             do {
                 let jsonDecoder = JSONDecoder()
                 if let data = data {
-                let decodedRes = try jsonDecoder.decode(DataResponse.self, from: data)
+                    let decodedRes = try jsonDecoder.decode(DataResponse.self, from: data)
                     DispatchQueue.main.async {
                         for item in decodedRes.radios {
                             self.radios.append(item)
-//                            self.playOneAyah(linkURL: item.radio_url)
+                            
                         }
                         self.tableView.reloadData()
-                // self.quran = decoder
-               // print(self.quran.radios)
-//                print(" Data recieved: \(decodedRes)")
-//                for item in self.quran.radios{
-//                    print(item.)
-                }
+                        
+                    }
                 }
             }catch{
                 print(error.localizedDescription)
-//                print("unable to fetch \(err?.localizedDescription)")
             }
         }
         task.resume()
     }
 }
-
-//class Quran : Codable{
-//    var radios : [Information]
-//
-//}
-//struct DataResponse : Codable{
-//    let radios: [Radio]
-//}
-//struct Radio:Codable {
-//    var name : String
-//    let radio_url:String
-//}
-
-class MyCell : UITableViewCell{
-    @IBOutlet var nameLbl : UILabel!
-}
-
